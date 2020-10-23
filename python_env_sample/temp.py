5ffc01e2d17519f37b601b8213c6ca4bceb6fb20
rasmit40 <62358784+rasmit40@users.noreply.github.com>
2020-10-22 23:11:32 -0400

    start of sample training program following tutorial

---
+++ python_env_sample/temp.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+import matplotlib.pylot as plt
+
+import tensorflow as tf
+
+
+train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
+
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                           origin=train_dataset_url)
+
+print("Local copy of the dataset file: {}".format(train_dataset_fp))
+
+!head -n5 {train_dataset_fp}
+
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+feature_names = column_names[:-1]
+label_name = column_names[-1]
+
+print("Features: {}".format(feature_names))
+print("Label: {}".format(label_name))
