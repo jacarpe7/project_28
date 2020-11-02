81426913438b31434702b177c617d6b13c71c4d9
rasmit40 <62358784+rasmit40@users.noreply.github.com>
2020-11-02 10:25:38 -0300

    added way that allows for the recording and output for 50 length, once delta gets up to 40 or higher

--- python_scripts/readIntoObject.py
+++ python_scripts/readIntoObject.py
@@ -3,7 +3,7 @@ import serial
 # This script uses the hardware build in folder qt7-ascii-out-dylan
 
 # Need to define port according to your setup. Typical port name - Windows: 'COM3'  Mac: '/dev/tty.usbmodem12345'
-serialPort = serial.Serial(port='/dev/tty.usbmodem14702',baudrate=38400,bytesize=8,timeout=2,stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port='COM3',baudrate=38400,bytesize=8,timeout=2,stopbits=serial.STOPBITS_ONE)
 
 # define sample object to hold data
 class Row:
@@ -27,8 +27,27 @@ f.write(headers)
 # discard first line
 serialPort.readline()
 
-# read in data from serial, set to 1000 lines
-for x in range(1000):
+val0 = 0
+val1 = 0
+val2 = 0
+val3 = 0
+val4 = 0
+i = 0
+
+#read in data from serial, set to 50 lines
+while val0 < 40 and val1 < 40 and val2 < 40 and val3 < 40 and val4 < 40 :
+    parseLine = serialPort.readline().decode('utf-8').split(",")
+    list.append(Row(i,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
+    i = i + 1
+    for obj in list:
+        val0 = int(obj.delta0)
+        val1 = int(obj.delta1)
+        val2 = int(obj.delta2)
+        val3 = int(obj.delta3)
+        val4 = int(obj.delta4)
+    
+list.clear()
+for x in range(50):
     # parse readline() into array, can be easily placed into other objects if wanted
     parseLine = serialPort.readline().decode('utf-8').split(",")
     # time component is discarded and replaced with x variable increment; time output is 8-bit and resets after 255
