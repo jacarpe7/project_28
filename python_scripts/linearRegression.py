9f80cfc5926145a046d80397f73d3f92d7f66a81
Kelly <kellyjp09@gmail.com>
2020-11-05 18:12:14 -0600

    adds new models and compares

--- python_scripts/linearRegression.py
+++ python_scripts/linearRegression.py
@@ -1,5 +1,13 @@
+import pandas
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 from numpy import genfromtxt
 
 my_data = genfromtxt('rs-01.csv', delimiter=',')
@@ -14,6 +22,39 @@ y2 = np.array(my_data[:,3])
 x3 = np.array(my_data[:,3]).reshape(-1,1)
 y3 = np.array(my_data[:,4])
 
+seed = 7
+
+#prepare models
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, x, y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+
+
+'''
 #Creates Linear Regression model for each sensor 
 model = LinearRegression().fit(x,y)
 
@@ -34,3 +75,4 @@ print('slope:', model.coef_)
 y_pred = model.predict(x)
 
 print('predicted response:', y_pred, sep='\n')
+'''
