3c3633067805e776ddc52b1942704a7a46524c9d
rasmit40 <62358784+rasmit40@users.noreply.github.com>
2020-11-07 13:35:15 -0300

    now works as intended, console will ask how many attempts at a gesture is needed

--- python_scripts/readIntoObject.py
+++ python_scripts/readIntoObject.py
@@ -4,7 +4,7 @@ import serial
 
 
 print("How many gesture records would you like to make? ")
-amount = input();
+amount = int(input())
 # Need to define port according to your setup. Typical port name - Windows: 'COM3'  Mac: '/dev/tty.usbmodem12345'
 serialPort = serial.Serial(port='COM3',baudrate=38400,bytesize=8,timeout=2,stopbits=serial.STOPBITS_ONE)
 
@@ -21,45 +21,45 @@ class Row:
 # array to hold objects
 list = []
 
-for (int index = 0; i < amount; i++)
-# setup filewriting to CSV
-filename = "stream%s.csv" % index
-f = open(filename, "w")
-headers = "time,delta0,delta1,delta2,delta3,delta4\n"
-f.write(headers)
-
-# discard first line
-serialPort.readline()
-
-val0 = 0
-val1 = 0
-val2 = 0
-val3 = 0
-val4 = 0
-i = 0
-
-#read in data from serial, set to 50 lines
-while val0 < 40 and val1 < 40 and val2 < 40 and val3 < 40 and val4 < 40 :
-    parseLine = serialPort.readline().decode('utf-8').split(",")
-    list.append(Row(i,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
-    i = i + 1
-    for obj in list:
-        val0 = int(obj.delta0)
-        val1 = int(obj.delta1)
-        val2 = int(obj.delta2)
-        val3 = int(obj.delta3)
-        val4 = int(obj.delta4)
+for index in range(amount):
+    # setup filewriting to CSV
+    filename = "stream%s.csv" % index
+    f = open(filename, "w")
+    headers = "time,delta0,delta1,delta2,delta3,delta4\n"
+    f.write(headers)
     
-list.clear()
-for x in range(50):
-    # parse readline() into array, can be easily placed into other objects if wanted
-    parseLine = serialPort.readline().decode('utf-8').split(",")
-    # time component is discarded and replaced with x variable increment; time output is 8-bit and resets after 255
-    list.append(Row(x,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
-
-# write to CSV
-for obj in list:
-    f.write(str(obj.time) + "," + str(obj.delta0) + "," + str(obj.delta1) + "," + str(obj.delta2) + "," + str(obj.delta3) + "," + str(obj.delta4) + ",\n")
+    # discard first line
+    serialPort.readline()
+    
+    val0 = 0
+    val1 = 0
+    val2 = 0
+    val3 = 0
+    val4 = 0
+    i = 0
+    
+    #read in data from serial, set to 50 lines
+    while val0 < 40 and val1 < 40 and val2 < 40 and val3 < 40 and val4 < 40 :
+        parseLine = serialPort.readline().decode('utf-8').split(",")
+        list.append(Row(i,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
+        i = i + 1
+        for obj in list:
+            val0 = int(obj.delta0)
+            val1 = int(obj.delta1)
+            val2 = int(obj.delta2)
+            val3 = int(obj.delta3)
+            val4 = int(obj.delta4)
+        
+    list.clear()
+    for x in range(50):
+        # parse readline() into array, can be easily placed into other objects if wanted
+        parseLine = serialPort.readline().decode('utf-8').split(",")
+        # time component is discarded and replaced with x variable increment; time output is 8-bit and resets after 255
+        list.append(Row(x,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
+    
+    # write to CSV
+    for obj in list:
+        f.write(str(obj.time) + "," + str(obj.delta0) + "," + str(obj.delta1) + "," + str(obj.delta2) + "," + str(obj.delta3) + "," + str(obj.delta4) + ",\n")
 
 f.close()
 serialPort.close()
