cdd00e4bf3473db2c3497c2c48fa2f1cb6c6cbe7
Kelly <kellyjp09@gmail.com>
2020-11-07 15:53:40 -0600

    Updates new time classification file

--- python_scripts/timeSeriesClassification.py
+++ python_scripts/timeSeriesClassification.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-%matplotlib inline
+
 import matplotlib.pyplot as plt
 from os import listdir
 from keras.preprocessing import sequence
@@ -13,8 +13,22 @@ from keras.optimizers import Adam
 from keras.models import load_model
 from keras.callbacks import ModelCheckpoint
 
-df1 = pd.read_csv(‘/MovementAAL/dataset/MovementAAL_RSS_1.csv')
-df2 = pd.read_csv('/MovementAAL/dataset/MovementAAL_RSS_2.csv')
+df1 = pd.read_csv('MovementAAL/dataset/MovementAAL_RSS_1.csv')
+df2 = pd.read_csv('MovementAAL/dataset/MovementAAL_RSS_2.csv')
 
 df1.head()
 df2.head()
+
+df1.shape, df2.shape
+
+path = 'MovementAAL/dataset/MovementAAL_RSS_'
+sequences = list()
+for i in range (1,59):
+    file_path = path + str(i) + '.csv'
+    print(file_path)
+    df = pd.read_csv(file_path, header=0)
+    values = df.values
+    sequences.append(values)
+
+targets = pd.read_csv('MovementAAL/dataset/MovementAAL_target.csv')
+targets = targets.values[:,1]
