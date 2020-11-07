494c1fc30cc5aae3049eb8fb0825a921b166bbef
Kelly <kellyjp09@gmail.com>
2020-11-06 20:22:15 -0600

    removes old data and updates linearRegression file

--- python_scripts/linearRegression.py
+++ python_scripts/linearRegression.py
@@ -1,13 +1,12 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import make_scorer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.model_selection import cross_validate
+import pandas as pd 
+import numpy as np 
+import pickle 
+import matplotlib.pyplot as plt 
+from scipy import stats 
+import tensorflow as tf 
+import seaborn as sns 
+from sklearn import metrics 
+from sklearn.model_selection import train_test_split
 from numpy import genfromtxt
 
 my_data = genfromtxt('rs-01.csv', delimiter=',')
@@ -20,120 +19,4 @@ y = np.array(my_data[:,2])
 
 df = pd.read_csv('rs-01.csv')
 
-df.head()
-
-# Import required library for resampling
-from imblearn.under_sampling import RandomUnderSampler
-
-# Instantiate Random Under Sampler
-rus = RandomUnderSampler(random_state=42)
-
-# Perform random under sampling
-df_data, df_target = rus.fit_resample(X, y)
-
-# Visualize new classes distributions
-sns.countplot(df_target).set_title('Balanced Data Set')
-
-# Define dictionary with performance metrics
-scoring = {'accuracy':make_scorer(accuracy_score), 
-           'precision':make_scorer(precision_score),
-           'recall':make_scorer(recall_score), 
-           'f1_score':make_scorer(f1_score)}
-
-# Import required libraries for machine learning classifiers
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-
-# Instantiate the machine learning classifiers
-log_model = LogisticRegression(max_iter=10000)
-svc_model = LinearSVC(dual=False)
-dtr_model = DecisionTreeClassifier()
-rfc_model = RandomForestClassifier()
-gnb_model = GaussianNB()
-
-# Define the models evaluation function
-def models_evaluation(X, y, folds):
-    
-    '''
-    X : data set features
-    y : data set target
-    folds : number of cross-validation folds
-    
-    '''
-    
-    # Perform cross-validation to each machine learning classifier
-    log = cross_validate(log_model, X, y, cv=folds, scoring=scoring)
-    svc = cross_validate(svc_model, X, y, cv=folds, scoring=scoring)
-    dtr = cross_validate(dtr_model, X, y, cv=folds, scoring=scoring)
-    rfc = cross_validate(rfc_model, X, y, cv=folds, scoring=scoring)
-    gnb = cross_validate(gnb_model, X, y, cv=folds, scoring=scoring)
-
-    # Create a data frame with the models perfoamnce metrics scores
-    models_scores_table = pd.DataFrame({'Logistic Regression':[log['test_accuracy'].mean(),
-                                                               log['test_precision'].mean(),
-                                                               log['test_recall'].mean(),
-                                                               log['test_f1_score'].mean()],
-                                       
-                                      'Support Vector Classifier':[svc['test_accuracy'].mean(),
-                                                                   svc['test_precision'].mean(),
-                                                                   svc['test_recall'].mean(),
-                                                                   svc['test_f1_score'].mean()],
-                                       
-                                      'Decision Tree':[dtr['test_accuracy'].mean(),
-                                                       dtr['test_precision'].mean(),
-                                                       dtr['test_recall'].mean(),
-                                                       dtr['test_f1_score'].mean()],
-                                       
-                                      'Random Forest':[rfc['test_accuracy'].mean(),
-                                                       rfc['test_precision'].mean(),
-                                                       rfc['test_recall'].mean(),
-                                                       rfc['test_f1_score'].mean()],
-                                       
-                                      'Gaussian Naive Bayes':[gnb['test_accuracy'].mean(),
-                                                              gnb['test_precision'].mean(),
-                                                              gnb['test_recall'].mean(),
-                                                              gnb['test_f1_score'].mean()]},
-                                      
-                                      index=['Accuracy', 'Precision', 'Recall', 'F1 Score'])
-    
-    # Add 'Best Score' column
-    models_scores_table['Best Score'] = models_scores_table.idxmax(axis=1)
-    
-    # Return models performance metrics scores data frame
-    return(models_scores_table)
-  
-# Run models_evaluation function
-models_evaluation(df_data, df_target, 2)
-
-
-'''
-x2 = np.array(my_data[:,2]).reshape(-1,1)
-y2 = np.array(my_data[:,3])
-
-x3 = np.array(my_data[:,3]).reshape(-1,1)
-y3 = np.array(my_data[:,4])
-
-#Creates Linear Regression model for each sensor 
-model = LinearRegression().fit(x,y)
-
-sensor2 = LinearRegression().fit(x2,y2)
-
-sensor3 = LinearRegression().fit(x3,y3)
-
-
-#scores model, intercept, coefficient, and prediction
-r_sq = model.score(x,y)
-
-print('coefficient of determination', r_sq)
-
-print('intercept:', model.intercept_)
-
-print('slope:', model.coef_)
-
-y_pred = model.predict(x)
-
-print('predicted response:', y_pred, sep='\n')
-'''
+df.head(5)
