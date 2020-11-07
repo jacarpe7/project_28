0694207f96962d0a88a528a42765dffa1bd38837
Kelly <kellyjp09@gmail.com>
2020-11-07 16:05:36 -0600

    updates python file

--- python_scripts/timeSeriesClassification.py
+++ python_scripts/timeSeriesClassification.py
@@ -32,3 +32,56 @@ for i in range (1,59):
 
 targets = pd.read_csv('MovementAAL/dataset/MovementAAL_target.csv')
 targets = targets.values[:,1]
+
+groups = pd.read_csv('MovementAAL/groups/MovementAAL_DatasetGroup.csv', header=0)
+groups = groups.values[:,1]
+
+len_sequences = []
+for one_seq in sequences:
+    len_sequences.append(len(one_seq))
+print(pd.Series(len_sequences).describe())
+
+
+#Padding the sequence with the values in last row to max length
+to_pad = 119
+new_seq = []
+for one_seq in sequences:
+    len_one_seq = len(one_seq)
+    last_val = one_seq[-1]
+    n = to_pad - len_one_seq
+   
+    to_concat = np.repeat(one_seq[-1], n).reshape(5, n).transpose()
+    new_one_seq = np.concatenate([one_seq, to_concat])
+    new_seq.append(new_one_seq)
+final_seq = np.stack(new_seq)
+
+#truncate the sequence to length 60
+from keras.preprocessing import sequence
+seq_len = 90
+final_seq=sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
+
+
+train = [final_seq[i] for i in range(len(groups)) if (groups[i]==2)]
+validation = [final_seq[i] for i in range(len(groups)) if groups[i]==1]
+test = [final_seq[i] for i in range(len(groups)) if groups[i]==3]
+
+train_target = [targets[i] for i in range(len(groups)) if (groups[i]==2)]
+validation_target = [targets[i] for i in range(len(groups)) if groups[i]==1]
+test_target = [targets[i] for i in range(len(groups)) if groups[i]==3]
+train = np.array(train)
+validation = np.array(validation)
+test = np.array(test)
+
+train_target = np.array(train_target)
+train_target = (train_target+1)/2
+
+validation_target = np.array(validation_target)
+validation_target = (validation_target+1)/2
+
+test_target = np.array(test_target)
+test_target = (test_target+1)/2
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(seq_len, 4)))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
