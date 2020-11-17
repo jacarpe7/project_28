7eaf1ef4911567aea99b41507fd1f9854b3ebccb
djohns91 <djohns91@asu.edu>
2020-11-16 19:12:01 -0800

    US92-Task107 modified sound output program and uploaded sample mp3s generated

--- audio_generation/generateAudio.py
+++ audio_generation/generateAudio.py
@@ -30,11 +30,16 @@ f.write(headers)
 
 # Create an empty AudioSegment
 result = AudioSegment.silent(duration=0)
+sound1 = AudioSegment.from_wav("1.wav")
+sound2 = AudioSegment.from_wav("2.wav")
+sound3 = AudioSegment.from_wav("3.wav")
+sound4 = AudioSegment.from_wav("4.wav")
+sound5 = AudioSegment.from_wav("5.wav")
 
 # discard first line
 serialPort.readline()
 
-# read in data from serial, set to 1000 lines
+# read in data from serial, set to 1000 lines - start at 100 for first use
 for x in range(1000):
     # parse readline() into array, can be easily placed into other objects if wanted
     parseLine = serialPort.readline().decode('utf-8').split(",")
@@ -42,10 +47,23 @@ for x in range(1000):
     list.append(Row(x,parseLine[1],parseLine[2],parseLine[3],parseLine[4],parseLine[5]))
     for n in range(1,6):
     	if (int(parseLine[n]) > 50):
-    		gen = Sine(int(parseLine[n]) * 3 * n * n)
-    		sine = gen.to_audio_segment(duration=100).apply_gain(-2).fade_in(50).fade_out(100)
-    		# Append the sine to our result
-    		result += sine
+            if (n == 1):
+                sine = sound1.apply_gain(-2).fade_in(150).fade_out(150)
+            elif n == 2:
+                sine = sound2.apply_gain(-2).fade_in(150).fade_out(150)
+                #sine = gen.to_audio_segment(duration=150).apply_gain(-2).fade_in(150).fade_out(150)
+            elif n == 3:
+                sine = sound3.apply_gain(-2).fade_in(150).fade_out(150)
+                #sine = gen.to_audio_segment(duration=150).apply_gain(-2).fade_in(50).fade_out(150)
+            elif n == 4:
+                sine = sound4.apply_gain(-2).fade_in(150).fade_out(150)
+                #sine = gen.to_audio_segment(duration=100).apply_gain(-2).fade_in(100).fade_out(50)
+            elif n == 5:
+                sine = sound5.apply_gain(-2).fade_in(150).fade_out(150)
+                #sine = gen.to_audio_segment(duration=100).apply_gain(-2).fade_in(100).fade_out(100)
+    		
+            # Append the sine to our result
+            result += sine
 
 # write to CSV
 for obj in list:
