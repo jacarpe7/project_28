0152ebb0416ed1f0187ce4eff5e26082bbbe9709
Kelly <kellyjp09@gmail.com>
2020-12-04 18:13:14 -0600

    updates template model

---
+++ python_scripts/data_processor.py
@@ -0,0 +1,18 @@
+#needed imports from Pandas, Numpy, Pickle, Matplot, Keras
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from os import listdir
+from keras.preprocessing import sequence
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.optimizers import Adam
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+
+#loading the exported pkl model and testing the accuracy score
+model = load_model('best_model.pkl')
+from sklearn.metrics import accuracy_score
