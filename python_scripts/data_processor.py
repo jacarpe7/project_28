5f88113d3264f67b13b8d1e64714845891546da7
Kelly <kellyjp09@gmail.com>
2021-01-16 13:14:09 -0600

    Starts Jupyter Notebook

--- python_scripts/data_processor.py
+++ python_scripts/data_processor.py
@@ -13,6 +13,16 @@ from keras.optimizers import Adam
 from keras.models import load_model
 from keras.callbacks import ModelCheckpoint
 
+#loading raw data
+path = 'MovementAAL/dataset/MovementAAL_RSS_'
+sequences = list()
+for i in range (1,1):
+    file_path = path + str(i) + '.csv'
+    print(file_path)
+    df = pd.read_csv(file_path, header=0)
+    values = df.values
+    sequences.append(values)
+
 #loading the exported pkl model and testing the accuracy score
 model = load_model('best_model.pkl')
 
