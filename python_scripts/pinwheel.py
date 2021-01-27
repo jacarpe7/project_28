aa41fc73b4680cd0a2d3eb8517aeb3f36a611be8
djohns91 <djohns91@asu.edu>
2021-01-26 18:39:43 -0800

    US191-Task201
    Up selection gesture data and revised pinwheel.py script

--- python_scripts/pinwheel.py
+++ python_scripts/pinwheel.py
@@ -10,30 +10,35 @@ serialPort.readline()
 
 i = 0
 
-while i < 10:
+while i < 50:
      parseLine = serialPort.readline().decode('utf-8').split(",")
-     # remove time value, alternatively we can reprogram the microcontroller to not output it
-     
-     print(parseLine)
 
+     # isolate deltas for activity recognition
      deltas = [str(parseLine[3]), str(parseLine[7]), str(parseLine[11]), str(parseLine[15])]
-
-     print(deltas)
 
      # convert string array to int array
      map(int, deltas)
+
      # grab max delta value
      max_value = max(deltas)
+
      # if max delta value exceeds threshold, read in data to get gesture 'data signature'
-     if int(max_value) > 35:
+     if int(max_value) > 30:
         filename = "stream_" + str(i) + ".csv"
         f = open(filename, "w")
-        headers = "ch1,sig1,ref1,delta1,ch2,sig2,ref2,delta2,ch3,sig3,ref3,delta3,ch4,sig4,ref4,delta4\n"
+        # for debugging
+        # headers = "ch1,sig1,ref1,delta1,ch2,sig2,ref2,delta2,ch3,sig3,ref3,delta3,ch4,sig4,ref4,delta4\n"
+        # deltas only
+        headers = "delta1,delta2,delta3,delta4\n"
         f.write(headers)
-        for x in range(50):
+        for x in range(40):
             parseLine = serialPort.readline().decode('utf-8').split(",")
-            f.write(str(parseLine[0]) + "," + str(parseLine[1]) + "," + str(parseLine[2]) + "," + deltas[0] + "," + str(parseLine[4]) + "," + str(parseLine[5]) + "," + str(parseLine[6]) + "," + deltas[1] + "," + str(parseLine[8]) + "," + str(parseLine[9]) + "," + str(parseLine[10]) + "," + deltas[2] + "," + str(parseLine[12]) + "," + str(parseLine[13]) + "," + str(parseLine[14]) + "," + deltas[3] + "\n")
-            # f.write(str(parseLine[0]) + "," + str(parseLine[1]) + "," + str(parseLine[2]) + "," + str(parseLine[3]) + "," + str(parseLine[4]) + "," + str(parseLine[5]) + "," + str(parseLine[6]) + "," + str(parseLine[7]) + "\n")
+            
+            # delta values only
+            f.write(str(parseLine[3]) + "," + str(parseLine[7]) + "," + str(parseLine[11]) + "," + str(parseLine[15]) + "\n")
+
+            # for debugging, includes all data output
+            # f.write(str(parseLine[0]) + "," + str(parseLine[1]) + "," + str(parseLine[2]) + "," + str(parseLine[3]) + "," + str(parseLine[4]) + "," + str(parseLine[5]) + "," + str(parseLine[6]) + "," + str(parseLine[7]) + "," + str(parseLine[8]) + "," + str(parseLine[9]) + "," + str(parseLine[10]) + "," + str(parseLine[11]) + "," + str(parseLine[12]) + "," + str(parseLine[13]) + "," + str(parseLine[14]) + "," + str(parseLine[15]) + "\n")
             
         i += 1
         f.close()
