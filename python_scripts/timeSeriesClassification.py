7b696b0873ac362f6d8f1050cb93e8bde11b3d99
Kelly <kellyjp09@gmail.com>
2021-01-31 22:56:09 -0600

    Updates model generation to reflect new dataset

--- python_scripts/timeSeriesClassification.py
+++ python_scripts/timeSeriesClassification.py
@@ -25,15 +25,22 @@ sequences = list()
 
 for i in range (1,9):
     file_path = path + str(i) + '.csv'
-    df = pd.read_csv(file_path, header=0, usecols=["delta0", "delta1", "delta2", "delta3", "delta4"])
+    df = pd.read_csv(file_path, header=0, usecols=["delta1", "delta2", "delta3", "delta4"])
     values = df.values
     sequences.append(values)
 
 path = 'MovementAAL/new_dataset/stream_000'
 
-for i in range(10, 59):
+for i in range(10, 99):
     file_path = path + str(i) + '.csv'
-    df = pd.read_csv(file_path, header=0, usecols=["delta0", "delta1", "delta2", "delta3", "delta4"])
+    df = pd.read_csv(file_path, header=0, usecols=["delta1", "delta2", "delta3", "delta4"])
+    sequences.append(values)
+
+path = 'MovementAAL/new_dataset/stream_00'
+
+for i in range(100, 299):
+    file_path = path + str(i) + '.csv'
+    df = pd.read_csv(file_path, header=0, usecols=["delta1", "delta2", "delta3", "delta4"])
     sequences.append(values)
 
 
@@ -42,7 +49,7 @@ targets = pd.read_csv('MovementAAL/new_dataset/MovementAAL_target.csv')
 targets = targets.values[:,1]
 
 #Loading grouping
-groups = pd.read_csv('MovementAAL/newgroups/MovementAAL_DatasetGroup.csv', header=0)
+groups = pd.read_csv('MovementAAL/groups/MovementAAL_DatasetGroup.csv', header=0)
 groups = groups.values[:,1]
 
 print(len(groups))
@@ -54,38 +61,38 @@ print(pd.Series(len_sequences).describe())
 
 
 #Padding the sequence with the values in last row to max length
-to_pad = 30
+to_pad = 55
 new_seq = []
 for one_seq in sequences:
     len_one_seq = len(one_seq)
     last_val = one_seq[-1]
     n = to_pad - len_one_seq
    
-    to_concat = np.repeat(one_seq[-1], n).reshape(5, n).transpose()
+    to_concat = np.repeat(one_seq[-1], n).reshape(4, n).transpose()
     new_one_seq = np.concatenate([one_seq, to_concat])
     new_seq.append(new_one_seq)
 final_seq = np.stack(new_seq)
 
 #truncate the sequence to length 60
 from keras.preprocessing import sequence
-seq_len = 30
+seq_len = 50
 final_seq=sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
 
-print(groups)
+print(len(groups))
 
 #Training data based on group 2
 train = [final_seq[i] for i in range(len(groups)) if (groups[i]==2)]
 #validation data based on group 1
 validation = [final_seq[i] for i in range(len(groups)) if groups[i]==1]
 #test data based on group 3
-test = [final_seq[i] for i in range(len(groups)-1) if groups[i]==3]
+test = [final_seq[i] for i in range(len(groups)-5) if groups[i]==3]
 
 #train target based on group 2
 train_target = [targets[i] for i in range(len(groups)) if (groups[i]==2)]
 #validation target based on group 1
 validation_target = [targets[i] for i in range(len(groups)) if groups[i]==1]
 #test target based on group 3
-test_target = [targets[i] for i in range(len(groups)) if groups[i]==3]
+test_target = [targets[i] for i in range(len(groups-100)-5) if groups[i]==3]
 
 #creating np.arrays for each dataset
 train = np.array(train)
@@ -94,21 +101,21 @@ test = np.array(test)
 
 #training target data
 train_target = np.array(train_target)
-#train_target = (train_target+1)/2
+train_target = (train_target+1)/2
 
 #validation target
 validation_target = np.array(validation_target)
-#validation_target = (validation_target+1)/2
+validation_target = (validation_target+1)/2
 
 #test data and target test
 test_target = np.array(test_target)
-#test_target = (test_target+1)/2
+test_target = (test_target+1)/2
 
 print(train_target)
 
 #adding the LSTM to the model and printing the summary
 model = Sequential()
-model.add(LSTM(120, input_shape=(seq_len, 5)))
+model.add(LSTM(120, input_shape=(seq_len, 4)))
 model.add(Dense(1, activation='sigmoid'))
 model.summary()
 print(model.summary())
