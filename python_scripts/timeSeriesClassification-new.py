77dda66bfe5ba5978d77ff988863a4ee4b646405
djohns91 <djohns91@asu.edu>
2021-02-25 16:29:36 -0800

    US236-Task241 modified data intake for model building
    accuracy improved, but need to determine why it is insufficient for gesture recognition

--- python_scripts/timeSeriesClassification-new.py
+++ python_scripts/timeSeriesClassification-new.py
@@ -16,10 +16,11 @@ from keras.callbacks import ModelCheckpoint
 #loading raw data
 path = 'new_model/dataset-new/Null/stream_'
 sequences = list()
+cols_to_use = [0,1,2,3]
 for i in range (200):
     file_path = path + str(i) + '.csv'
     print(file_path)
-    df = pd.read_csv(file_path, header=0)
+    df = pd.read_csv(file_path, usecols=cols_to_use , header=0, index_col=None)
     values = df.values
     sequences.append(values)
 
@@ -27,7 +28,7 @@ path = 'new_model/dataset-new/LeftSwipe/stream_'
 for i in range (200):
     file_path = path + str(i) + '.csv'
     print(file_path)
-    df = pd.read_csv(file_path, header=0)
+    df = pd.read_csv(file_path, usecols=cols_to_use , header=0, index_col=None)
     values = df.values
     sequences.append(values)
 
@@ -35,7 +36,7 @@ path = 'new_model/dataset-new/RightSwipe/stream_'
 for i in range (200):
     file_path = path + str(i) + '.csv'
     print(file_path)
-    df = pd.read_csv(file_path, header=0)
+    df = pd.read_csv(file_path, usecols=cols_to_use , header=0, index_col=None)
     values = df.values
     sequences.append(values)
 
@@ -43,7 +44,7 @@ path = 'new_model/dataset-new/FullActivity/stream_'
 for i in range (200):
     file_path = path + str(i) + '.csv'
     print(file_path)
-    df = pd.read_csv(file_path, header=0)
+    df = pd.read_csv(file_path, usecols=cols_to_use , header=0, index_col=None)
     values = df.values
     sequences.append(values)
 
@@ -51,49 +52,29 @@ for i in range (200):
 targets = pd.read_csv('new_model/dataset-new/Movement_target.csv')
 targets = targets.values[:,1]
 
+print(targets)
+
 #Loading grouping
-groups = pd.read_csv('new_model/groups/Movement_DatasetGroup.csv', header=0)
+groups = pd.read_csv('new_model/dataset-new/groups/Movement_DatasetGroup.csv', header=0)
 groups = groups.values[:,1]
 
-len_sequences = []
-for one_seq in sequences:
-    len_sequences.append(len(one_seq))
-print(pd.Series(len_sequences).describe())
+print(groups)
 
-
-#Padding the sequence with the values in last row to max length
-to_pad = 800
-new_seq = []
-for one_seq in sequences:
-    len_one_seq = len(one_seq)
-    last_val = one_seq[-1]
-    n = to_pad - len_one_seq
-   
-    to_concat = np.repeat(one_seq[-1], n).reshape(4, n).transpose()
-    new_one_seq = np.concatenate([one_seq, to_concat])
-    new_seq.append(new_one_seq)
-final_seq = np.stack(new_seq)
-
-#truncate the sequence to length 60
+#truncate the sequence to length 30
 from keras.preprocessing import sequence
 seq_len = 30
-final_seq=sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
+
+print(sequences[1])
 
 #Training data based on group 2
-train = [final_seq[i] for i in range(len(groups)) if (groups[i]==2)]
-#validation data based on group 1
-validation = [final_seq[i] for i in range(len(groups)) if groups[i]==1]
-#test data based on group 3
-test = [final_seq[i] for i in range(len(groups)) if groups[i]==3]
+train = [sequences[i] for i in range(len(groups)) if (groups[i]==2)]
+validation = [sequences[i] for i in range(len(groups)) if groups[i]==1]
+test = [sequences[i] for i in range(len(groups)) if groups[i]==3]
 
 #train target based on group 2
 train_target = [targets[i] for i in range(len(groups)) if (groups[i]==2)]
-#validation target based on group 1
 validation_target = [targets[i] for i in range(len(groups)) if groups[i]==1]
-#test target based on group 3
 test_target = [targets[i] for i in range(len(groups)) if groups[i]==3]
-#test target based on group 4
-test_target = [targets[i] for i in range(len(groups)) if groups[i]==4]
 
 #creating np.arrays for each dataset
 train = np.array(train)
@@ -123,11 +104,11 @@ print(model.summary())
 adam = Adam(lr=0.001)
 chk = ModelCheckpoint('best_model.pkl', monitor='val_accuracy', save_best_only=True, mode='max', verbose=1)
 model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-model.fit(train, train_target, epochs=20, batch_size=800, callbacks=[chk], validation_data=(validation,validation_target))
+model.fit(train, train_target, epochs=5, batch_size=800, callbacks=[chk], validation_data=(validation,validation_target))
 
 #loading the exported pkl model and testing the accuracy score
 model = load_model('best_model.pkl')
 from sklearn.metrics import accuracy_score
-test_preds = model.predict_classes(test)
+test_preds = model.predict(test)
 accuracy_score(test_target, test_preds)
 print(accuracy_score(test_target, test_preds))
