4bb4b82f09c98db7bffb8f1d02f8cfc10b9d587e
djohns91 <djohns91@asu.edu>
2021-03-19 14:11:37 -0700

    US236-Task268 fixed data quantity

--- back_end/qt7/process_data/splitTrainTest.py
+++ back_end/qt7/process_data/splitTrainTest.py
@@ -1,11 +1,11 @@
 # n refers to sensors 1 through 4
 for n in range (5):
-	r = open("sensor" + str(n+1) + ".txt", "r")
+	r = open("dataBySensor/sensor" + str(n+1) + ".txt", "r")
 	# one file each for train and test sets
 	f1 = open("sensor" + str(n+1) + "-train.txt", "w")
 	f2 = open("sensor" + str(n+1) + "-test.txt", "w")
 	# v refers to the number of lines in each sensorN text file
-	for v in range (1,101):
+	for v in range (1,301):
 		# write to train set for 3 of every 4 lines
 		if (v % 4 != 0):
 			f1.write(r.readline())
